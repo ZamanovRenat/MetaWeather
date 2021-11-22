eb79fc965bdcb1c3fc6f8f97da2561a398194c6e
ZamanovRenat <zamanov.renat@mail.ru>
2021-11-22 20:15:44 +0300

    Создание класса для работы с HTTP клиентом

---
+++ MetaWeather/MetaWeatherClient.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+
+namespace MetaWeather
+{
+    public class MetaWeatherClient
+    {
+        private readonly HttpClient _client;
+
+        public MetaWeatherClient(HttpClient Client)
+        {
+            _client = Client;
+        }
+    }
+}
