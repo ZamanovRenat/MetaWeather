11406e3b17f02920eeed1781f8f3482ee8571444
ZamanovRenat <zamanov.renat@mail.ru>
2021-11-22 20:04:30 +0300

    Подключение хостинга

--- Tests/MetaWeather.TestConsole/Program.cs
+++ Tests/MetaWeather.TestConsole/Program.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace MetaWeather.TestConsole
 {
     class Program
     {
+        private static IHost _hosting;
+
+        public static IHost Hosting => _hosting ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(ConfigureServices);
+
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection service)
+        {
+            
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
